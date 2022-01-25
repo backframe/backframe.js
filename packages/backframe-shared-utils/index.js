ea39d5d031a33a2da75b9ad48decf1cca9c143d1
Victor Ndaba <mwangindaba@gmail.com>
2022-01-25 21:44:00 +0300

    feat(bf-shared-utils): exported utils from the shared utils
    exported utils from the shared utils

--- packages/backframe-shared-utils/index.js
+++ packages/backframe-shared-utils/index.js
@@ -1,5 +1,11 @@
 const RestGenerator = require("./src/RestGenerator");
+const { toTitleCase, pluralize } = require("./lib/utils");
+const { writeMultiple, writeSingle } = require("./lib/writeFileTree");
 
 module.exports = {
   RestGenerator,
+  toTitleCase,
+  pluralize,
+  writeMultiple,
+  writeSingle,
 };
