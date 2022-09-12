16ed2f2715692923ee5099aef878f6e89600d3d1
Victor Ndaba <mwangindaba@gmail.com>
2022-09-12 21:03:49 +0300

    refactor: removed bf-dbs pkg

--- packages/backframe-dbs/lib/utils/resolveOptions.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports.getURI = (options) => {
-  const ctx = options.rootDir || process.cwd();
-  require("dotenv").config({ path: path.join(ctx, ".env") });
-  const DB_URI = process.env.DB_URI;
-
-  return DB_URI;
-};
