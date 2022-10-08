328fda7d9dd5ae8a395971005f2976eaf7942ea3
Victor Ndaba <mwangindaba@gmail.com>
2022-10-08 23:15:04 +0300

    feat(utils): started work on the utils functionality(logger)

---
+++ packages/backframe-utils/src/logger.ts
@@ -0,0 +1,20 @@
+import winston from "winston";
+
+export const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    new winston.transports.File({ filename: "bf-error.log", level: "error" }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+} else {
+  logger.add(new winston.transports.File({ filename: "bf-combined.log" }));
+}
