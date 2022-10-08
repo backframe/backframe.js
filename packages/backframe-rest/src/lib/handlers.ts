619ec3225b94c433d395f5b61085806e7b35e682
Victor Ndaba <mwangindaba@gmail.com>
2022-10-08 21:41:32 +0300

    refactor(rest): updated rest handlers functionality

--- packages/backframe-rest/src/lib/handlers.ts
+++ packages/backframe-rest/src/lib/handlers.ts
@@ -1,33 +1,58 @@
-import { IHandlerContext, IRouteConfig } from "@backframe/core";
+import { BfRequestHandler, IModuleConfig, MethodName } from "@backframe/core";
 
-const defaultHandler = (ctx: IHandlerContext) => {
-  ctx.res.status(200).json({
-    status: "Success",
-    msg: "Basic route functionality working",
-    body: "This is a default static route handler. You can either define a model for this route or override the specific handler to add functionality.",
-  });
-};
-
-export function _generateHandlers(module: IRouteConfig) {
-  if (module.config?.model) {
-    return _dynamicHandlers(module);
-  }
-  return _staticHandlers(module);
+class Handler {
+  constructor(
+    public method: MethodName,
+    public action: BfRequestHandler,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    public input?: any
+  ) {}
 }
 
-function _staticHandlers(module: IRouteConfig) {
+class HandlersConfig {
+  constructor(private handlers: Handler[]) {}
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const handlers: any = {};
-  module.config?.enabledMethods?.map((m) => {
-    handlers[m] = defaultHandler;
-  });
-  return handlers;
+  handle(method: MethodName, cfg: { input?: any; action: BfRequestHandler }) {
+    this.handlers.push(new Handler(method, cfg.action, cfg.input));
+    return this;
+  }
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function _dynamicHandlers(_module: IRouteConfig) {
-  return {
-    get: defaultHandler,
-    post: defaultHandler,
-  };
+export function defineHandlers() {
+  return new HandlersConfig([]);
+}
+
+export function _parseHandlers(module: IModuleConfig, route: string) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const values: any = {};
+  const handlers = module.default?.handlers ?? [];
+
+  handlers.map((h: Handler) => {
+    values[h.method] = h.action;
+  });
+
+  if (!Object.keys(values).length) {
+    // generate default handlers for enabled methods
+    const enabled = module.config?.enabled ?? getDefaultEnabled();
+
+    // TODO: Implement dynamic handlers where model is involved
+    enabled.map((method: string) => {
+      values[method] = generateHandler(method, route);
+    });
+  }
+
+  return values;
+}
+
+function getDefaultEnabled() {
+  return ["get", "getOne", "put", "post", "delete"];
+}
+
+function generateHandler(method: string, route: string) {
+  return () => ({
+    status: "Okay",
+    msg: `The "${method}" method for the \`${route}\` route is working successfully.`,
+    body: `This is a default static handler. It can be overriden by defining your own handler for the "${method}" method or by defining a model in the config of the \`${route}\` route.`,
+  });
 }
