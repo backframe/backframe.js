1301ed84f54c341f9fc77a79bfeacde3b6ef0514
Victor Ndaba <mwangindaba@gmail.com>
2022-10-09 21:40:07 +0300

    refactor(cli): updated the `bf serve` command

--- packages/backframe-cli/src/cmd/serve.ts
+++ packages/backframe-cli/src/cmd/serve.ts
@@ -1,15 +1,42 @@
-import loadCfg from "@backframe/core";
+import loadCfg, { IBfServer } from "@backframe/core";
+import { logger } from "@backframe/utils";
+import chokidar from "chokidar";
 import path from "path";
 
+const current = (...s: string[]) => path.join(process.cwd(), ...s);
+const load = async (s: string) => await import(`file://${s}`);
+
 export default async function serve() {
-  const pkgPath = resolvePackagePath("@backframe/rest");
-  console.log(pkgPath);
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const module = require(`${pkgPath}/dist/index.js`);
-  const config = await loadCfg();
-  module.default(config);
+  let app: IBfServer;
+
+  const start = async () => {
+    const pkgPath = resolvePackagePath("@backframe/rest");
+    const module = await load(`${pkgPath}/dist/index.js`);
+    const config = await loadCfg();
+    app = await module.default(config);
+  };
+
+  await start();
+  const watcher = chokidar.watch(["src", "bf.config.js"], {
+    persistent: false,
+  });
+
+  watcher.on("ready", () => {
+    logger.info("watching for file changes...");
+
+    watcher.on("all", () => {
+      logger.warn("changes detected, restarting server...");
+      restart();
+    });
+  });
+
+  const restart = () => {
+    app.stop(() => {
+      start();
+    });
+  };
 }
 
 function resolvePackagePath(name: string) {
-  return path.join(process.cwd(), "node_modules", name);
+  return current("node_modules", name);
 }
