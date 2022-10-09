08b5268f6b703a4aa96ffc4625b912153184c537
Victor Ndaba <mwangindaba@gmail.com>
2022-10-09 23:43:22 +0300

    refactor(utils): updated and expanded logger setup and cfg

--- packages/backframe-utils/src/logger.ts
+++ packages/backframe-utils/src/logger.ts
@@ -1,20 +1,32 @@
-import winston from "winston";
+import winston, { createLogger, format, transports } from "winston";
+const { printf, colorize, combine, timestamp } = format;
 
-export const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  defaultMeta: { service: "user-service" },
-  transports: [
-    new winston.transports.File({ filename: "bf-error.log", level: "error" }),
-  ],
+const fmt = printf(({ level, message, timestamp }) => {
+  return `${timestamp} [${level}] - ${message}`;
 });
 
-if (process.env.NODE_ENV !== "production") {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    })
-  );
-} else {
-  logger.add(new winston.transports.File({ filename: "bf-combined.log" }));
+export const logger =
+  process.env.NODE_ENV === "production" ? getProdLogger() : getDevLogger();
+
+function getDevLogger() {
+  return createLogger({
+    level: "debug",
+    format: combine(
+      colorize({ all: true }),
+      timestamp({ format: "HH:mm:ss" }),
+      fmt
+    ),
+    transports: [new transports.Console()],
+  });
+}
+
+function getProdLogger() {
+  return createLogger({
+    level: "info",
+    format: combine(timestamp(), fmt),
+    transports: [
+      new winston.transports.File({ filename: "bf-error.log", level: "error" }),
+      new winston.transports.File({ filename: "backframe.log" }),
+    ],
+  });
 }
