42e8f83d1bacc452abd7efe13f1aec4f6456b9fa
Victor Ndaba <mwangindaba@gmail.com>
2022-10-09 23:13:00 +0300

    refactor(cli): updated cli commands

--- packages/backframe-cli/src/index.ts
+++ packages/backframe-cli/src/index.ts
@@ -1,19 +1,29 @@
 #!/usr/bin/env node
 import { program } from "commander";
 import serve from "./cmd/serve.js";
+import watch from "./cmd/watch.js";
 
 program
-  .name("bf")
-  .version("0.1.0")
+  .name("bf-js")
+  .version("0.0.0")
   .description(
     "A framework for rapid development and deployment of APIs/backends"
   );
 
 program
   .command("serve")
-  .description("Serve local bf project")
+  .alias("s")
+  .description("Serve the project in the current working directory")
   .action(() => {
     serve();
+  });
+
+program
+  .command("watch")
+  .alias("w")
+  .description("Start the app server in watch mode")
+  .action(() => {
+    watch();
   });
 
 program.parse();
