d026d3e19c232a56e97c36ae70d56c95e00ad8a8
Victor Ndaba <mwangindaba@gmail.com>
2022-10-09 23:10:59 +0300

    refactor(cli): refactored server cmd, removed fs watcher

--- packages/backframe-cli/src/cmd/serve.ts
+++ packages/backframe-cli/src/cmd/serve.ts
@@ -1,40 +1,16 @@
 import loadCfg, { IBfServer } from "@backframe/core";
-import { logger } from "@backframe/utils";
-import chokidar from "chokidar";
 import path from "path";
 
 const current = (...s: string[]) => path.join(process.cwd(), ...s);
 const load = async (s: string) => await import(`file://${s}`);
 
 export default async function serve() {
-  let app: IBfServer;
+  const pkgPath = resolvePackagePath("@backframe/rest");
+  const module = await load(`${pkgPath}/dist/index.js`);
+  const config = await loadCfg();
+  const app: IBfServer = await module.default(config);
 
-  const start = async () => {
-    const pkgPath = resolvePackagePath("@backframe/rest");
-    const module = await load(`${pkgPath}/dist/index.js`);
-    const config = await loadCfg();
-    app = await module.default(config);
-  };
-
-  await start();
-  const watcher = chokidar.watch(["src", "bf.config.js"], {
-    persistent: false,
-  });
-
-  watcher.on("ready", () => {
-    logger.info("watching for file changes...");
-
-    watcher.on("all", () => {
-      logger.warn("changes detected, restarting server...");
-      restart();
-    });
-  });
-
-  const restart = () => {
-    app.stop(() => {
-      start();
-    });
-  };
+  return app;
 }
 
 function resolvePackagePath(name: string) {
