436ee9221eb9dbf485dcfd73d25718f9a5dc956c
Victor Ndaba <mwangindaba@gmail.com>
2022-10-11 20:35:23 +0300

    refactor(utils): updated dev logger

--- packages/backframe-utils/src/logger.ts
+++ packages/backframe-utils/src/logger.ts
@@ -2,18 +2,21 @@ import winston, { createLogger, format, transports } from "winston";
 const { printf, colorize, combine, timestamp } = format;
 
 const fmt = printf(({ level, message, timestamp }) => {
-  return `${timestamp} [${level}] - ${message}`;
+  return `[${timestamp}] ${level} - ${message}`;
 });
 
 export const logger =
   process.env.NODE_ENV === "production" ? getProdLogger() : getDevLogger();
 
 function getDevLogger() {
+  const fmt = printf(({ level, message, timestamp }) => {
+    return `[${timestamp}] ${getPadding(level)} -  ${message}`;
+  });
   return createLogger({
-    level: "debug",
+    level: process.env.BF_LOG_LEVEL || "debug",
     format: combine(
       colorize({ all: true }),
-      timestamp({ format: "HH:mm:ss" }),
+      timestamp({ format: "YY-MM-DD HH:mm:ss.Ms" }),
       fmt
     ),
     transports: [new transports.Console()],
@@ -22,11 +25,22 @@ function getDevLogger() {
 
 function getProdLogger() {
   return createLogger({
-    level: "info",
+    level: process.env.BF_LOG_LEVEL || "info",
     format: combine(timestamp(), fmt),
     transports: [
       new winston.transports.File({ filename: "bf-error.log", level: "error" }),
       new winston.transports.File({ filename: "backframe.log" }),
     ],
   });
+}
+
+function getPadding(val: string) {
+  const max = 15;
+  const diff = max - val.length;
+
+  for (let i = diff; i > 0; i--) {
+    val += " ";
+  }
+
+  return val;
 }
