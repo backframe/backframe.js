fe49a393607cd1e80ef619d4fd674c29a7ab7a34
Victor Ndaba <mwangindaba@gmail.com>
2022-10-11 20:09:16 +0300

    refactor(admin): updated plugin script to log after loaded

--- packages/backframe-admin/scripts/index.js
+++ packages/backframe-admin/scripts/index.js
@@ -1,5 +1,6 @@
 const { definePlugin } = require("@backframe/core");
 const { ssr } = require("./utils");
+const { logger } = require("@backframe/utils");
 
 const p = definePlugin();
 
@@ -7,7 +8,14 @@ p.beforeServerStart = async (cfg) => {
   process.env.BF_DEBUG && console.log("beforeServerStart called");
   const app = cfg.server._app;
   await ssr(app);
+  // TODO: pass db and auth objects to plugin
   return cfg;
+};
+
+p.afterServerStart = (cfg) => {
+  logger.debug(
+    `admin ui ready on http://localhost:${cfg.server.cfg.port}/admin`
+  );
 };
 
 module.exports = p;
