6690a4f5dda0ab966813e391ff3dc891e58e40f4
Victor Ndaba <mwangindaba@gmail.com>
2022-10-11 20:07:24 +0300

    chore(admin): updated dev script

--- packages/backframe-admin/scripts/dev.js
+++ packages/backframe-admin/scripts/dev.js
@@ -7,7 +7,7 @@ ssr(app)
   .then(() => {
     const PORT = process.env.PORT || 5173;
     app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
+      console.log(`dev-server running on http://localhost:${PORT}`);
     });
   })
   .catch(console.error);
