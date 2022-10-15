34b664b5ccb68e9400664cf9d784d03499ce3048
Victor Ndaba <mwangindaba@gmail.com>
2022-10-15 22:11:00 +0300

    feat(admin): added nested routing for the app

--- packages/backframe-admin/src/router.tsx
+++ packages/backframe-admin/src/router.tsx
@@ -4,16 +4,29 @@ import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Other } from "./pages/Other";
 
-import { Login } from "~/pages/Login/Login";
+import App from "~/pages/app";
+import { Login } from "~/pages/login";
+import { Register } from "~/pages/register";
 import "./app.css";
-import { Register } from "./pages/Register/Register";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/auth/login" element={<Login />} />
-      <Route path="/auth/register" element={<Register />} />
+      <Route path="/auth">
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+      <Route path="/app" element={<App />}>
+        <Route path="metrics" element={<Other />} />
+        <Route path="analytics" element={<Other />} />
+        <Route path="logs" element={<Other />} />
+        <Route path="auth" element={<Other />} />
+        <Route path="database" element={<Other />} />
+        <Route path="plugins" element={<Other />} />
+        <Route path="storage" element={<Other />} />
+        <Route path="interfaces" element={<Other />} />
+      </Route>
       <Route path="/other" element={<Other />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
