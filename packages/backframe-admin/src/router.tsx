b9dad9cad4d4c33d5f44827aaa97e6c797a51dec
Victor Ndaba <mwangindaba@gmail.com>
2022-10-16 22:49:13 +0300

    feat(admin): updated app router

--- packages/backframe-admin/src/router.tsx
+++ packages/backframe-admin/src/router.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 
-import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Other } from "./pages/Other";
 
@@ -8,24 +7,28 @@ import App from "~/pages/app";
 import { Login } from "~/pages/login";
 import { Register } from "~/pages/register";
 import "./app.css";
+import { Index } from "./pages";
+import Auth from "./pages/app/auth/auth";
+import Overview from "./pages/app/overview/overview";
+import Plugins from "./pages/app/plugins/plugins";
+import Storage from "./pages/app/storage/storage";
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Index />} />
       <Route path="/auth">
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
       </Route>
       <Route path="/app" element={<App />}>
-        <Route path="metrics" element={<Other />} />
+        <Route path="overview/*" element={<Overview />} />
         <Route path="analytics" element={<Other />} />
-        <Route path="logs" element={<Other />} />
-        <Route path="auth" element={<Other />} />
+        <Route path="auth/*" element={<Auth />} />
         <Route path="database" element={<Other />} />
-        <Route path="plugins" element={<Other />} />
-        <Route path="storage" element={<Other />} />
-        <Route path="interfaces" element={<Other />} />
+        <Route path="plugins/*" element={<Plugins />} />
+        <Route path="storage/*" element={<Storage />} />
+        <Route path="resources" element={<Other />} />
       </Route>
       <Route path="/other" element={<Other />} />
       <Route path="*" element={<NotFound />} />
