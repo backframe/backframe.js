c8e4e7cfb2e96ed4e7dc0f0c71e2881093af81b2
Victor Ndaba <mwangindaba@gmail.com>
2022-10-25 22:33:25 +0300

    refactor(cli): updated `bf serve` command

--- packages/backframe-cli/src/cmd/serve.ts
+++ packages/backframe-cli/src/cmd/serve.ts
@@ -1,18 +1,32 @@
 import loadCfg, { IBfServer } from "@backframe/core";
+import { logger, resolvePackage } from "@backframe/utils";
+import fs from "fs";
 import path from "path";
 
 const current = (...s: string[]) => path.join(process.cwd(), ...s);
-const load = async (s: string) => await import(`file://${s}`);
+const load = async (s: string) => await import(`${s}`);
 
-export default async function serve() {
-  const pkgPath = resolvePackagePath("@backframe/rest");
-  const module = await load(`${pkgPath}/dist/index.js`);
-  const config = await loadCfg();
-  const app: IBfServer = await module.default(config);
+export async function serve() {
+  const cfg = await loadCfg();
+  let server: IBfServer;
+  // search if there is a custom server file
+  const src = cfg.getFileSource();
+  const file = path.join(src, "server.js");
 
-  return app;
-}
-
-function resolvePackagePath(name: string) {
-  return current("node_modules", name);
+  if (fs.existsSync(file)) {
+    const module = await load(current(file));
+    if (!Object.keys(module.default).length) {
+      logger.error(`please export a default object from \`${"server.js"}\``);
+      process.exit(1);
+    }
+    server = module.default;
+  } else {
+    const pkg = resolvePackage("@backframe/rest");
+    server = pkg.defaultServer();
+  }
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  server.__initialize(cfg);
+  server.start();
+  return server;
 }
