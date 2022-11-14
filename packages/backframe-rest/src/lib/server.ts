e2f4a57fcc8b40d7aa8294407170d39dbeed0aac
Victor Ndaba <mwangindaba@gmail.com>
2022-11-14 23:07:13 +0300

    refactor(rest): updated server interface, simplified and cleanup

--- packages/backframe-rest/src/lib/server.ts
+++ packages/backframe-rest/src/lib/server.ts
@@ -1,99 +1,99 @@
-import type {
+import {
   BfConfig,
-  BfRequestHandler,
   BfResourceConfig,
   IHandlerContext,
-  Listener,
   MethodName,
 } from "@backframe/core";
 import { logger } from "@backframe/utils";
 import cors, { CorsOptions } from "cors";
 import express, {
-  ErrorRequestHandler,
   Express,
+  NextFunction,
   Request as ExpressReq,
   Response as ExpressRes,
 } from "express";
 import helmet from "helmet";
 import { Server as HttpServer } from "http";
+import { GenericException, NotFoundExeption } from "./errors.js";
 import { resolveRoutes } from "./routes.js";
 
-// cors options
-// request logger
-interface ServerConfig {
+interface IBfServerConfig {
   port?: number;
-  globalMiddleware?: BfRequestHandler[];
-  cors?: {
-    enabled: boolean;
-    options?: CorsOptions;
-  };
-  logRequests?: boolean;
+  enableCors?: boolean;
+  corsOptions?: CorsOptions;
+  enableLogger?: boolean;
 }
 
 export class BfServer {
-  public _app: Express;
-  public _handle!: HttpServer;
-  public _resources!: BfResourceConfig[];
-  private _appCfg!: BfConfig;
+  private _cfg!: IBfServerConfig;
+  private _app!: Express;
+  private _handle!: HttpServer;
+  private _bfConfig!: BfConfig;
+  private _middleware!: (() => void)[];
+  private _resources!: BfResourceConfig[];
 
-  constructor(public cfg: ServerConfig) {
+  constructor(cfg: IBfServerConfig) {
+    this._cfg = cfg;
+    this._middleware = [];
     this._app = express();
   }
 
-  async __initialize(cfg: BfConfig) {
+  async _initialize(cfg: BfConfig) {
     cfg._setServer(this);
-    this._appCfg = cfg;
+    this._bfConfig = cfg;
+    // TODO: apply middleware
+    // TODO: load resources
+    // TODO: Invoke plugins beforeStart
+    // TODO: setup error handlers
+    this._applyMiddleware();
     const resources = await resolveRoutes(cfg);
     resources.forEach((r) => {
       this.addResource(r as BfResourceConfig);
     });
     this._resources = resources;
-
-    // invoke plugins before server starts
-    cfg._invokePlugins("beforeServerStart");
-    return cfg;
+    cfg._invokeListeners("beforeServerStart");
+    this._setupErrHandlers();
   }
 
-  async __resolveRoutes(cfg: BfConfig) {
-    const resources = await resolveRoutes(cfg);
-    resources.forEach((r) => {
-      this.addResource(r as BfResourceConfig);
-    });
-    this._resources = resources;
-  }
-
-  public start(port = this.cfg.port) {
-    this.applyMiddleware();
-    this._handle = this._app.listen(port, () => {
-      const host = `http://localhost:${port}`;
-      logger.info(`server started on ${host}`);
-      if (this._appCfg._cfg.plugins?.includes("@backframe/admin" as Listener)) {
-        logger.info(`admin ui ready on ${host}/admin`);
-      }
-    });
-  }
-
-  public stop(callback?: (err?: Error | undefined) => void | undefined) {
-    this._handle.close(callback);
-  }
-
-  private applyMiddleware() {
+  private _applyMiddleware() {
     this._app.use(helmet());
-    this.cfg.cors?.enabled && this._app.use(cors(this.cfg.cors.options ?? {}));
-    if (this.cfg.logRequests) this._app.use(RequestLogger());
+    const useCors = this._cfg.enableCors;
+    const log = this._cfg.enableLogger;
 
-    const errHandler: ErrorRequestHandler = (err, req, res, next) => {
-      res.status(404).json({
-        statusCode: 404,
-        error: "Not Found",
-        msg: `Cannot ${req.method} ${req.path}`,
+    useCors && this._app.use(cors(this._cfg.corsOptions));
+    log &&
+      this._app.use((req, res, next) => {
+        const start = Date.now();
+        next();
+        logger.http(
+          `${req.method} ${req.originalUrl} HTTP/${req.httpVersion} -> ${
+            res.statusCode
+          } in ${Date.now() - start}ms`
+        );
       });
-    };
-    this._app.use(errHandler);
   }
 
-  private _wrapHandler(method: MethodName, r: BfResourceConfig) {
-    return (req: ExpressReq, res: ExpressRes) => {
+  private _setupErrHandlers() {
+    // at this point, no route has been found
+    this._app.use("*", (_req, _res, _next) => {
+      throw NotFoundExeption();
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-implicit-any
+    this._app.use(
+      (
+        err: GenericException,
+        _req: ExpressReq,
+        res: ExpressRes,
+        _next: NextFunction
+      ) => {
+        res.status(err.statusCode).json(err.getValues());
+      }
+    );
+  }
+
+  private _generateHandler(method: MethodName, r: BfResourceConfig) {
+    return (req: ExpressReq, res: ExpressRes, next: NextFunction) => {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       const ctx: IHandlerContext = {
         _req: req,
@@ -106,17 +106,32 @@ export class BfServer {
       };
 
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const val = r.handlers[method as MethodName]!(ctx);
-      const t = typeof val === "string" ? "text/html" : "application/json";
-      return res.status(200).setHeader("Content-Type", t).json(val);
+      const value = r.handlers[method as MethodName]!(ctx);
+
+      if (value instanceof GenericException) {
+        next(value);
+      }
+      const t = typeof value === "string" ? "text/html" : "application/json";
+      return res.status(200).setHeader("Content-Type", t).json(value);
     };
   }
 
+  async start(port = this._cfg.port || 6969) {
+    this._handle = this._app.listen(port, () => {
+      // TODO: expose flags etc...
+      const host = `http://localhost:${port}`;
+      logger.info(`server started on ${host}`);
+      if (this._bfConfig._includesPlugin("@backframe/admin"))
+        logger.info(`admin ui ready on ${host}/admin`);
+    });
+  }
+
+  stop(callback?: (err?: Error | undefined) => void | undefined) {
+    this._handle.close(callback);
+  }
+
   addResource(r: BfResourceConfig) {
-    const middleware = r.routeConfig.middleware ?? [
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (_rq: any, _rs: any, next: () => any) => next(),
-    ];
+    const middleware = r.routeConfig.middleware ?? [];
     const methods = Object.keys(r.handlers);
 
     methods.forEach((m) => {
@@ -124,43 +139,28 @@ export class BfServer {
         this._app.get(
           `${r.route}/:id`,
           middleware,
-          this._wrapHandler("getOne", r)
+          this._generateHandler("getOne", r)
         );
       } else {
         type value = "get" | "post" | "put" | "delete" | "patch";
         this._app[m as value](
           r.route,
           middleware,
-          this._wrapHandler(m as MethodName, r)
+          this._generateHandler(m as MethodName, r)
         );
       }
     });
   }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  addMiddleware(m: any) {
-    this._app.use(m);
-  }
-}
-
-export function createServer(cfg: ServerConfig) {
-  return new BfServer(cfg);
 }
 
 export function defaultServer() {
   return new BfServer({
     port: 6969,
-    logRequests: true,
-    cors: {
-      enabled: true,
-    },
+    enableCors: true,
+    enableLogger: true,
   });
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const RequestLogger = () => (req: ExpressReq, res: ExpressRes, next: any) => {
-  logger.http(
-    `${req.method} ${req.path} HTTP/${req.httpVersion} -> ${res.statusCode}`
-  );
-  next();
-};
+export function createServer(cfg: IBfServerConfig) {
+  return new BfServer(cfg);
+}
