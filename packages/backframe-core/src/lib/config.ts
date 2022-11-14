37475a5855b4e0f9df12d0a61e6f5f9a3b100fb2
Victor Ndaba <mwangindaba@gmail.com>
2022-11-14 22:59:47 +0300

    refactor(core): heavy refactor to config parsing process
    modularized config parsing process and simplified plugins interface

--- packages/backframe-core/src/lib/config.ts
+++ packages/backframe-core/src/lib/config.ts
@@ -11,20 +11,21 @@ import fs from "fs";
 import pkg from "glob";
 import { generateDefaultConfig } from "../utils/index.js";
 import { BfConfigSchema, BfUserConfig, IBfServer } from "../utils/types.js";
-import { Listener, PluginFn, PluginsConfig } from "./plugins.js";
 const { glob } = pkg;
+
+const pkgJson = require(resolveCwd("package.json"));
+const format = pkgJson.type === "module" ? "esm" : "cjs";
 
 export async function loadConfig() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   let module: any;
-  let config: BfUserConfig;
-  const pkg = require(resolveCwd("package.json"));
-  const format = pkg.type === "module" ? "esm" : "cjs";
+  let userCfg: BfUserConfig;
   const matches = glob.sync("./bf.config.*");
 
+  // TODO: Hash and cache and compare config files
   if (!matches.length) {
     logger.warn("no config file found, using default backframe config");
-    config = generateDefaultConfig();
+    userCfg = generateDefaultConfig();
   } else {
     const m = matches[0];
     const shouldCompile = () => {
@@ -46,8 +47,8 @@ export async function loadConfig() {
   }
 
   const exported = module?.default ?? module;
-  config = Object.assign(generateDefaultConfig(), exported);
-  const opts = BfConfigSchema.safeParse(config);
+  userCfg = Object.assign(generateDefaultConfig(), exported);
+  const opts = BfConfigSchema.safeParse(userCfg);
   if (!opts.success) {
     logger.error("your config file is not valid");
     process.exit(1);
@@ -62,8 +63,8 @@ export async function loadConfig() {
     logger.info(`loaded env variables from \`${env[0]}\``);
   } // Step 2 done: loaded env vars for use in later stages
 
-  const expanded = new BfConfig(config);
-  const plugins = expanded._cfg.plugins ?? [];
+  const bfConfig = new BfConfig(userCfg);
+  const plugins = bfConfig._userCfg.plugins ?? [];
 
   if (plugins.length) {
     // console animations?!
@@ -72,20 +73,116 @@ export async function loadConfig() {
       let pkg;
       if (typeof plugin == "string") pkg = plugin;
       else pkg = plugin.resolve;
-      const module = resolvePackage(pkg);
 
-      Object.keys(module.listeners).forEach((m) => {
-        expanded._registerPlugins(m as Listener, module.listeners[m]);
-      });
+      // load and execute default export from pkg
+      resolvePackage(pkg)(bfConfig);
     }
 
-    expanded._invokePlugins("afterLoadConfig");
+    bfConfig._invokeListeners("afterLoadConfig");
   } // Step 4 done: Loaded plugins and invoke for current stage
 
-  if (expanded._hasPlugins("overrideBuilder")) {
-    expanded._invokePlugins("overrideBuilder");
-  } else {
-    const dir = config.settings.srcDir ?? "src";
+  bfConfig._buildFiles(); // Step 5 done: built typescript files or esm files
+
+  return bfConfig;
+}
+
+interface IListeners {
+  beforeServerStart?: PluginFn[];
+  afterLoadConfig?: PluginFn[];
+}
+
+type Event = "beforeServerStart" | "afterLoadConfig";
+export type PluginFn = (cfg: BfConfig) => BfConfig | void;
+export class BfConfig {
+  private fileSrc?: string;
+  private server!: IBfServer;
+  private _builder!: () => void | undefined;
+  private _listeners!: IListeners;
+
+  constructor(public _userCfg: BfUserConfig) {
+    this._builder = defaultBuilder(this);
+    const events = ["beforeServerStart", "afterLoadConfig"];
+    const listeners: IListeners = {};
+    events.forEach((e) => (listeners[e as Event] = []));
+    this._listeners = listeners;
+  }
+
+  set builder(builder: () => void | undefined) {
+    this._builder = builder;
+  }
+
+  _buildFiles() {
+    this._builder();
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  _setServer(server: any) {
+    this.server = server;
+  }
+
+  _updateFileSrc(src: string) {
+    this.fileSrc = src;
+  }
+
+  _invokeListeners(event: Event) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    for (const l of this._listeners[event]!) {
+      l(this);
+    }
+  }
+
+  _includesPlugin(name: string) {
+    const plugins = this._userCfg.plugins ?? [];
+    return plugins.some((p) => {
+      if (typeof p === "object") return p.resolve === name;
+      return p === name;
+    });
+  }
+
+  getFileSource() {
+    return this.fileSrc ?? this._userCfg.settings.srcDir;
+  }
+
+  getServer() {
+    return this.server._app;
+  }
+
+  getRestConfig() {
+    return this._userCfg.interfaces?.rest;
+  }
+
+  getGraphqlConfig() {
+    return this._userCfg.interfaces?.graphql;
+  }
+
+  getPluginOptions(plugin: string) {
+    let config: object = {};
+    this._userCfg.plugins?.forEach((p) => {
+      if (typeof p === "object" && p.resolve === plugin) {
+        config = p.options;
+      }
+    });
+    return config;
+  }
+
+  getProviderOptions(provider: string) {
+    let config: object = {};
+    this._userCfg.providers?.forEach((p) => {
+      if (p.provider === provider) config = p.config;
+    });
+    return config;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  addListener(event: Event, cb: () => any) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    this._listeners[event]!.push(cb);
+  }
+}
+
+function defaultBuilder(cfg: BfConfig) {
+  return () => {
+    const dir = cfg.getFileSource() ?? "src";
     const files = glob.sync(`./${dir}/**/*.ts`);
     const shouldBuild = () => {
       return format === "esm" || files.length;
@@ -98,10 +195,10 @@ export async function loadConfig() {
         entryPoints: [...glob.sync(`./${dir}/**/*.{ts,js}`)],
       });
     }
-    expanded._updateFileSrc(".bf");
+    cfg._updateFileSrc(".bf");
 
     if (format === "esm") {
-      const newPkg = Object.assign(pkg, {
+      const newPkg = Object.assign(pkgJson, {
         type: "commonjs",
       });
 
@@ -110,80 +207,5 @@ export async function loadConfig() {
         JSON.stringify(newPkg, null, 2)
       );
     }
-  } // Step 5 done: built typescript files or esm files
-
-  return expanded;
-}
-
-// TODO: rename to bfcfg
-export class BfConfig {
-  private plugins: PluginsConfig;
-  private fileSrc?: string;
-  private server!: IBfServer;
-
-  constructor(public _cfg: BfUserConfig) {
-    this.plugins = new PluginsConfig({
-      afterLoadConfig: [],
-      authClient: [],
-      beforeServerStart: [],
-      databaseClient: [],
-      overrideBuilder: [],
-      storageProvider: [],
-    });
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  _setServer(server: any) {
-    this.server = server;
-  }
-
-  _updateFileSrc(src: string) {
-    this.fileSrc = src;
-  }
-
-  _invokePlugins(key: Listener) {
-    this.plugins.invokePlugins(key, this);
-  }
-
-  _registerPlugins(key: Listener, fn: PluginFn) {
-    this.plugins.registerPlugin(key, fn);
-  }
-
-  _hasPlugins(key: Listener) {
-    return this.plugins.includesPlugins(key);
-  }
-
-  getFileSource() {
-    return this.fileSrc ?? this._cfg.settings.srcDir;
-  }
-
-  getServer() {
-    return this.server._app;
-  }
-
-  getRestConfig() {
-    return this._cfg.interfaces?.rest;
-  }
-
-  getGraphqlConfig() {
-    return this._cfg.interfaces?.graphql;
-  }
-
-  getPluginOptions(plugin: string) {
-    let config: object = {};
-    this._cfg.plugins?.forEach((p) => {
-      if (typeof p === "object" && p.resolve === plugin) {
-        config = p.options;
-      }
-    });
-    return config;
-  }
-
-  getProviderOptions(provider: string) {
-    let config: object = {};
-    this._cfg.providers?.forEach((p) => {
-      if (p.provider === provider) config = p.config;
-    });
-    return config;
-  }
+  };
 }
