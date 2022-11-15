d0a412d7ab7e58d1b7f1f212eef38ad7aa1dc779
Victor Ndaba <mwangindaba@gmail.com>
2022-11-15 23:40:04 +0300

    refactor(admin): updated plugin to use new plugin interface

--- packages/backframe-admin/scripts/index.js
+++ packages/backframe-admin/scripts/index.js
@@ -1,11 +1,19 @@
-const { definePlugin } = require("@backframe/core");
+// const { definePlugin } = require("@backframe/core");
 const { ssr } = require("./utils");
+const { logger } = require("@backframe/utils");
 
-const p = definePlugin();
+// const p = definePlugin();
 
-p.register("beforeServerStart", async (cfg) => {
-  process.env.BF_DEBUG && console.log("beforeServerStart called");
-  await ssr(cfg.getServer());
-});
+// p.register("beforeServerStart", async (cfg) => {
+//   process.env.BF_DEBUG && console.log("beforeServerStart called");
+//   await ssr(cfg.getServer());
+// });
 
-module.exports = p;
+module.exports = (cfg) => {
+  cfg.addListener("beforeServerStart", async () => {
+    await ssr(cfg.getInternalApp());
+  });
+  cfg.addListener("afterServerStart", () => {
+    logger.info(`admin ui ready on ${cfg.getServer().getHost()}/admin`);
+  });
+};
