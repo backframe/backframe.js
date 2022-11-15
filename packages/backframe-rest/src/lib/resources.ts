7a5d3635548c476a487a85cedb122b35e65cfe08
Victor Ndaba <mwangindaba@gmail.com>
2022-11-15 23:47:14 +0300

    feat(rest): created new resource loader module

---
+++ packages/backframe-rest/src/lib/resources.ts
@@ -0,0 +1,132 @@
+import { BfConfig, Methods, Model } from "@backframe/core";
+import { logger, resolveCwd } from "@backframe/utils";
+import { Response as ExpressRes } from "express";
+import pkg from "glob";
+import path from "path";
+import { Context } from "./context.js";
+import { Handler, _parseHandlers } from "./handlers.js";
+const { glob } = pkg;
+
+export type BfRequestHandler = (
+  ctx: Context
+) => string | object | string[] | ExpressRes;
+export interface BfResourceConfig {
+  route: string;
+  handlers: IResourceHandlers;
+  config: IRouteConfig;
+}
+
+export interface IResourceHandlers {
+  create?: Handler;
+  read?: Handler;
+  update?: Handler;
+  delete?: Handler;
+}
+
+export interface IRouteConfig {
+  model?: Model;
+  middleware?: BfRequestHandler[];
+  enabled?: Methods[];
+  public?: Methods[];
+}
+
+export class Resource {
+  private bfConfig: BfConfig;
+  private _route!: string;
+  private _handlers!: IResourceHandlers;
+  private _model?: Model;
+  private _middleware?: BfRequestHandler[];
+  private _mwMap: string[];
+
+  get route() {
+    return this._route;
+  }
+
+  get handlers() {
+    return this._handlers;
+  }
+
+  get model() {
+    return this._model;
+  }
+
+  get middleware() {
+    return this._middleware;
+  }
+
+  constructor(
+    file: string,
+    config: { bfConfig: BfConfig; middleware: string[] }
+  ) {
+    this.bfConfig = config.bfConfig;
+    this._mwMap = config.middleware;
+    this._middleware = [];
+
+    this.resolveName(file);
+    this.loadMiddleware(file);
+    this.loadHandlers(file);
+  }
+
+  private resolveName(match: string) {
+    let segments: string[] = [];
+    const bfConfig = this.bfConfig;
+    const urlPrefix = bfConfig.getRestConfig()?.urlPrefix ?? "/";
+
+    let file = match.replace(`./${bfConfig.getFileSource()}/routes`, "");
+    file = path.join(urlPrefix, file);
+
+    const name = path.basename(file);
+    const parts = name.split(".");
+    parts.pop(); // remove ext;
+
+    parts.forEach((p) => {
+      segments.push(p.replace("$", ":").replace("index", " "));
+    });
+
+    this._route = path
+      .join(path.dirname(file), segments.join("/"))
+      .replace(/\\/g, "/");
+  }
+
+  private loadMiddleware(file: string) {
+    const base = path.basename(file).split(".")[0];
+    let mw = this._mwMap.filter((v) => v.includes(base));
+    if (mw.length > 0) {
+      const module = require(resolveCwd(mw[0]));
+      Object.keys(module).forEach((fn) => {
+        this._middleware?.push(module[fn]);
+      });
+    }
+  }
+
+  private loadHandlers(file: string) {
+    const module = require(resolveCwd(file));
+    const config: IRouteConfig = Object.assign(
+      generateRouteConfig(),
+      module.config ?? {}
+    );
+
+    this._model = config.model;
+    this._middleware!.push(...(config.middleware ?? []));
+    this._handlers = _parseHandlers(module, this._route);
+  }
+}
+
+export async function loadResources(bfConfig: BfConfig) {
+  // TODO: Duplicate routes found
+  // first, traverse dir tree and get route files
+  const src = bfConfig.getFileSource();
+  let matches = glob.sync(`./${src}/routes/**/*.js`);
+
+  !matches.length && logger.warn("no files detected in the routes directory");
+  const middleware = matches.filter((m) => m.includes("_"));
+  matches = matches.filter((m) => !m.includes("_"));
+  return matches.map((m) => new Resource(m, { bfConfig, middleware }));
+}
+
+function generateRouteConfig(): IRouteConfig {
+  return {
+    enabled: ["create", "read", "update", "delete"],
+    public: ["read"],
+  };
+}
