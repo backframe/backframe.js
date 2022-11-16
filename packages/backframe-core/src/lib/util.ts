b6e87f887bb962ab93dbbd66e1802632a9d4ba9f
Victor Ndaba <mwangindaba@gmail.com>
2022-11-16 20:34:52 +0300

    refactor(core): condensed utils and types to single file

---
+++ packages/backframe-core/src/lib/util.ts
@@ -0,0 +1,51 @@
+import { z } from "zod";
+
+export const BfConfigSchema = z.object({
+  interfaces: z
+    .object({
+      rest: z
+        .object({
+          versioned: z.boolean().optional(),
+          urlPrefix: z.string().optional(),
+        })
+        .optional(),
+      graphql: z
+        .object({
+          mount: z.string().optional(),
+        })
+        .optional(),
+    })
+    .optional(),
+  plugins: z
+    .array(
+      z
+        .object({
+          resolve: z.string(),
+          options: z.any(),
+        })
+        .or(z.string())
+    )
+    .optional(),
+  providers: z
+    .array(
+      z.object({
+        provider: z.string(),
+        config: z.any(),
+      })
+    )
+    .optional(),
+  settings: z.object({
+    srcDir: z.string().optional(),
+    server: z.string().optional(),
+  }),
+});
+
+export type BfUserConfig = z.infer<typeof BfConfigSchema>;
+
+export function generateDefaultConfig(): BfUserConfig {
+  return {
+    settings: {
+      srcDir: "src",
+    },
+  };
+}
