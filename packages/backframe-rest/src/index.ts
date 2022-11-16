fcf155235455b7556da302870edcccefc56a6efe
Victor Ndaba <mwangindaba@gmail.com>
2022-11-16 20:42:56 +0300

    refactor(rest): updated lib exports

--- packages/backframe-rest/src/index.ts
+++ packages/backframe-rest/src/index.ts
@@ -1,5 +1,13 @@
-import { IRouteConfig } from "@backframe/core";
+import { IRouteConfig } from "./lib/util.js";
 
+export { Context } from "./lib/context.js";
+export {
+  ForbiddenException,
+  GenericException,
+  InternalException,
+  NotFoundExeption,
+  UnauthorizedException,
+} from "./lib/errors.js";
 export { defineHandlers } from "./lib/handlers.js";
 export { createServer, defaultServer } from "./lib/server.js";
 export const defineRouteConfig = (cfg: IRouteConfig) => cfg;
