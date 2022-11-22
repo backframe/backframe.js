1d2fe2abe4e37e9fd604a71d201e529583fcd681
Victor Ndaba <mwangindaba@gmail.com>
2022-11-22 23:11:31 +0300

    refactor(cli): updated the serve cmd

--- packages/backframe-cli/src/cmd/serve.ts
+++ packages/backframe-cli/src/cmd/serve.ts
@@ -1,33 +1,19 @@
 import loadCfg from "@backframe/core";
-import { logger, resolvePackage } from "@backframe/utils";
-import fs from "fs";
-import path from "path";
+import { resolvePackage } from "@backframe/utils";
+import { ensureBfProject } from "../lib/utils";
 
-const current = (...s: string[]) => path.join(process.cwd(), ...s);
-const load = async (s: string) => await import(`${s}`);
-
-export async function serve() {
+export async function serve(options: any) {
+  ensureBfProject();
   const cfg = await loadCfg();
   let server;
 
-  // search if there is a custom server file
-  const src = cfg.getFileSource() ?? "src";
-  const entry = cfg.getSettings().entryPoint ?? "server.js";
-  const file = path.join(src, entry);
-
-  if (fs.existsSync(file)) {
-    const module = await load(current(file));
-    if (!Object.keys(module.default).length) {
-      logger.error(`please export a default object from \`${"server.js"}\``);
-      process.exit(1);
-    }
-    server = module.default;
+  if (cfg.getServer()) {
+    server = cfg.getServer();
   } else {
     const pkg = resolvePackage("@backframe/rest");
     server = pkg.defaultServer();
   }
 
   server._initialize(cfg);
-  server.start();
-  return server;
+  server.start(options.port);
 }
