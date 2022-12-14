81d8335068c56b9637964e0b16daa5f59cc3fad7
Victor Ndaba <mwangindaba@gmail.com>
2022-12-14 21:49:56 +0300

    refactor(utils): updated resolvePkg method

--- packages/backframe-utils/src/pkg.ts
+++ packages/backframe-utils/src/pkg.ts
@@ -1,19 +1,18 @@
+import fs from "fs";
 /* eslint-disable @typescript-eslint/no-var-requires */
 import path from "path";
-
-export function resolvePackage(name: string) {
-  const current = (...s: string[]) =>
-    path.join(process.cwd(), "node_modules", ...s);
-  const pkg = require(current(name, "package.json"));
-  const main = pkg.main;
-  const module = require(current(name, main));
-  return module;
-}
 
 export function resolveCwd(...s: string[]) {
   return path.join(process.cwd(), ...s);
 }
 
 export async function loadModule(s: string) {
-  return await import(s);
+  return await import(`file://${s}`);
+}
+
+export async function resolvePackage(s: string) {
+  const p = resolveCwd("node_modules", s, "package.json");
+  const pkg = JSON.parse(fs.readFileSync(p, "utf-8"));
+  const module = await loadModule(resolveCwd("node_modules", s, pkg.main));
+  return module;
 }
