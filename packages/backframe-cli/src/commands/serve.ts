773b7ee08256d39c1fa68de75ba5d5cfda5f493b
Victor Ndaba <mwangindaba@gmail.com>
2022-12-16 01:21:49 +0300

    feat(cli): implemented `bf server` with fs watching

--- packages/backframe-cli/src/commands/serve.ts
+++ packages/backframe-cli/src/commands/serve.ts
@@ -1,4 +1,14 @@
-import { defineBfCommand } from "./index";
+import type { BfServer } from "@backframe/rest";
+import {
+  debug,
+  loadModule,
+  logger,
+  resolveCwd,
+  resolvePackage,
+} from "@backframe/utils";
+import chokidar from "chokidar";
+import { spawn } from "cross-spawn";
+import { defineBfCommand } from "./index.js";
 
 export default defineBfCommand({
   command: "serve",
@@ -17,9 +27,60 @@ export default defineBfCommand({
       .option("open", {
         alias: "o",
         boolean: true,
+        description: "Launch admin dashboard(if present)",
       });
   },
-  handler: (args) => {
-    console.log(args);
+  handler: async (_args) => {
+    const server = await getServer();
+
+    if (_args["watch"]) {
+      const watcher = chokidar.watch(["src/"], {
+        ignored: ["node_modules/", ".bf/"],
+      });
+
+      watcher.on("ready", () => {
+        logger.debug("watching for file changes...");
+        watcher.on("all", () => {
+          logger.debug("changes detected, restarting server... \n\n");
+          server._handle?.close(async () => {
+            await watcher.close();
+
+            const args = process.argv.slice(2);
+            spawn(resolveCwd("node_modules/.bin/bf"), args, {
+              env: process.env,
+              stdio: "inherit",
+            });
+          });
+        });
+      });
+    }
+
+    server.start(Number(_args["port"]));
   },
 });
+
+async function getServer() {
+  try {
+    const core = await resolvePackage("@backframe/core");
+    const config = await core.default();
+
+    const root = config.getRootDirName();
+    const entry = config.getEntryPointName();
+
+    const file = await loadModule(resolveCwd(root, entry));
+    if (!file.default) {
+      logger.error(`expected a default export from ${entry}`);
+      process.exit(1);
+    }
+
+    const server: BfServer = file.default;
+    await server.__init(config);
+
+    return server;
+  } catch (error) {
+    debug(error as string);
+    logger.error("an error occurred while trying to start the server");
+    logger.error("please make sure you have installed @backframe/core");
+    process.exit(1);
+  }
+}
