b8c652c38fafc06ee274d9b1519bd03957d82231
Victor Ndaba <mwangindaba@gmail.com>
2022-12-15 22:05:47 +0300

    refactor(core): updated pkg exports

--- packages/backframe-core/src/index.ts
+++ packages/backframe-core/src/index.ts
@@ -1,6 +1,4 @@
-// re-export to provide everything user needs in one framework
-export { z } from "zod";
 export { defineConfig, loadConfig as default } from "./config/config.js";
-export { BfConfig, PluginFn, PluginKey } from "./config/index.js";
-export { BfPluginConfig } from "./config/plugins.js";
+export { BfConfig, PluginKey } from "./config/index.js";
 export { BfUserConfig } from "./config/schema.js";
+export { BfPluginConfig, Plugin, PluginListener } from "./plugins/index.js";
