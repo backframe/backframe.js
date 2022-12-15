af26ea7389becccf9aa296648286fe460693fbb4
Victor Ndaba <mwangindaba@gmail.com>
2022-12-15 22:20:31 +0300

    refactor(rest): modified resource initialization
    updated imports to router, renamed loadHandlers -> initialize, updated handler parsing

--- packages/backframe-rest/src/app/resources.ts
+++ packages/backframe-rest/src/app/resources.ts
@@ -1,12 +1,20 @@
 import { BfConfig } from "@backframe/core";
-import { loadModule, logger, resolveCwd } from "@backframe/utils";
-import { globbySync } from "globby";
-import { Handler, IRouteConfig, Method } from "../lib/types.js";
-import { Item } from "../routing/index.js";
-import { ResourceHandlers, __getHandlers } from "./handlers.js";
+import { debug, loadModule, logger, resolveCwd } from "@backframe/utils";
+import {
+  Handler,
+  IHandlerConfig,
+  IModuleConfig,
+  IRouteConfig,
+  Method,
+} from "../lib/types.js";
+import { Item } from "../routing/router.js";
+import { ResourceHandlers, _crudToStd, _getStaticHandler } from "./handlers.js";
 
 export const DEFAULT_ENABLED: Method[] = ["create", "read", "update", "delete"];
 export const DEFAULT_PUBLIC: Method[] = ["read"];
+export const STD_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"];
+export type BfMethod = "GET" | "PUT" | "POST" | "PATCH" | "DELETE";
+export type ExpressMethods = "get" | "put" | "post" | "patch" | "delete";
 
 export class Resource<T> {
   #model?: T;
@@ -22,6 +30,7 @@ export class Resource<T> {
     this.#route = item.route;
     this.#bfConfig = config;
     this.#item = item;
+    this.#handlers = new ResourceHandlers();
   }
 
   get route() {
@@ -40,30 +49,56 @@ export class Resource<T> {
     return this.#handlers;
   }
 
-  async loadHandlers() {
-    const mod = await loadModule(resolveCwd(this.#item.filePath));
-    const config: IRouteConfig<T> = Object.assign(
-      {
-        enabled: DEFAULT_ENABLED,
-        public: DEFAULT_PUBLIC,
-      },
-      mod.config ?? {}
-    );
-    this.#model = config.model;
-    this.#public = config.public;
-    this.#enabled = config.enabled;
-    this.#handlers = __getHandlers(mod, this.#route);
-    this.#middleware = this.#handlers?.__middleware ?? [];
+  async initialize() {
+    try {
+      const mod = await loadModule(resolveCwd(this.#item.filePath));
+      const config: IRouteConfig<T> = Object.assign(
+        {
+          enabled: DEFAULT_ENABLED,
+          public: DEFAULT_PUBLIC,
+        },
+        mod.config ?? {}
+      );
+      this.#model = config.model;
+      this.#public = config.public;
+      this.#enabled = config.enabled;
+
+      this.#getHandlers(mod);
+    } catch (error) {
+      logger.error(
+        `an error occurred while trying to load resources at ${this.#route}`
+      );
+      debug(error);
+      process.exit(1);
+    }
   }
-}
 
-export function loadResources(cfg: BfConfig) {
-  const root = cfg.getRootDirName();
-  const src = cfg.getRoutesDirName();
-  const ptrn = `./${root}/${src}/**/*.js`;
+  // TODO: Validate file module
+  #getHandlers(module: IModuleConfig<unknown>) {
+    let h = module.default ?? new ResourceHandlers();
+    // find enabled methods
+    const enabled = module?.config?.enabled || DEFAULT_ENABLED;
 
-  const matches = globbySync(ptrn);
-  if (!matches.length) {
-    logger.warn(`no routes detected in the ${src} directory`);
+    enabled.forEach((e) => {
+      const m = _crudToStd(e);
+      if (!h[m]) h[m] = _getStaticHandler(e, this.#route);
+    });
+
+    Object.keys(h).forEach((k) => {
+      if (STD_METHODS.includes(k)) {
+        this.#handlers[k as BfMethod] = h[k as BfMethod];
+      }
+
+      if (k === "__middleware") {
+        this.#middleware = h[k];
+      }
+    });
+
+    // check named exports
+    STD_METHODS.forEach((m) => {
+      if (module[m]) {
+        this.#handlers[m as BfMethod] = module[m] as IHandlerConfig<{}>;
+      }
+    });
   }
 }
