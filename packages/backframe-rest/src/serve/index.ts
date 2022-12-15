7fafd3032cec4d9e4224aab26b8808ece1e7b89b
Victor Ndaba <mwangindaba@gmail.com>
2022-12-15 23:25:10 +0300

    refactor(rest): moved serve functionality

---
+++ packages/backframe-rest/src/serve/index.ts
@@ -0,0 +1,19 @@
+import { BfConfig } from "@backframe/core";
+import { loadModule, logger, resolveCwd } from "@backframe/utils";
+import { BfServer } from "../app/index.js";
+
+export async function startServer(config: BfConfig) {
+  const root = config.getRootDirName();
+  const entry = config.getEntryPointName();
+
+  const file = await loadModule(resolveCwd(root, entry));
+  if (!file.default) {
+    logger.error(`expected a default export from ${entry}`);
+    process.exit(1);
+  }
+
+  const server: BfServer = file.default;
+  await server.__init(config);
+
+  server.start();
+}
