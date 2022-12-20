d8337c7ec6cd18fc8eb6f7a350341708ce7530e2
Victor Ndaba <mwangindaba@gmail.com>
2022-12-20 23:21:18 +0300

    refactor: added ensureBfProject cmd

--- packages/backframe-cli/src/util.ts
+++ packages/backframe-cli/src/util.ts
@@ -1,3 +1,5 @@
+import { logger, require, resolveCwd } from "@backframe/utils";
+import { globbySync } from "globby";
 import yargs from "yargs";
 import add from "./commands/add";
 import _new from "./commands/new";
@@ -9,4 +11,23 @@ export function buildCommands(cli: yargs.Argv) {
   return [rest, serve, add, _new].forEach((c) =>
     cli.command(c.command, c.description, c.builder, c.handler)
   );
+}
+
+export function ensureBfProject() {
+  const pkg = require(resolveCwd("package.json"));
+  const deps = Object.keys(pkg.dependencies);
+
+  const exists = () => {
+    const matches = globbySync("bf.config.*");
+    if (matches.length) return true;
+    if (!deps.includes("@backframe/core")) return true;
+    return false;
+  };
+
+  if (!exists()) {
+    logger.error(
+      "This command can only be run from the root of a backframe project"
+    );
+    process.exit(10);
+  }
 }
