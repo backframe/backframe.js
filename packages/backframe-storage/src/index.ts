3c7659b67b9e6cd3392d67ace9ea22e885e01023
Victor Ndaba <mwangindaba@gmail.com>
2022-12-25 22:26:38 +0300

    feat(storage): started on bf storage plugin

---
+++ packages/backframe-storage/src/index.ts
@@ -0,0 +1,42 @@
+import type { BfConfig, BfPluginConfig } from "@backframe/core";
+import { logger } from "@backframe/utils";
+import multer from "multer";
+import { DEFAULT_CONFIG, IStorageConfig } from "./config.js";
+
+export default function (cfg: IStorageConfig = DEFAULT_CONFIG): BfPluginConfig {
+  return {
+    modifyServer(bfCfg) {
+      const storage = createStorageObject(cfg, bfCfg);
+      const upload = multer({ storage });
+      const app = bfCfg.server._app;
+
+      cfg.routes.forEach((r) => {
+        const { fileKey, route } = r;
+        app.post(route, upload.single(fileKey), (req, res) => {
+          const { file } = req;
+          // TODO: Send back url to file?
+          res.status(200).send({
+            msg: "File uploaded successfully",
+            file,
+          });
+        });
+        logger.info(`file uploads enabled on route: \`${r.route}\``);
+      });
+    },
+  };
+}
+
+function createStorageObject(store: IStorageConfig, _bfConfig: BfConfig) {
+  // TODO: Implement other storage engines
+  if (store.engine === "local") {
+    return multer.diskStorage({
+      destination: store.destination,
+      filename: function (_req, file, cb) {
+        const ext = file.originalname.split(".").pop();
+        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+        cb(null, `${file.fieldname}-${uniqueSuffix}.${ext}`);
+      },
+    });
+  }
+  throw new Error(`storage engine ${store.engine} unimplemented!!!`);
+}
