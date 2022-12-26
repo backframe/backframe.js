50a84623d637ac24bd7193daccf63d54576f5b3e
Victor Ndaba <mwangindaba@gmail.com>
2022-12-26 19:29:06 +0300

    feat(docs): created docs app

---
+++ docs/tailwind.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{svelte,js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ docs/vite.config.ts
@@ -0,0 +1,11 @@
+import postcss from "./postcss.config.cjs";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  css: {
+    postcss,
+  },
+});
