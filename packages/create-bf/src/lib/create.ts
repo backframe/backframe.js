35ba17a8f36d3ea6f70af9f6a2c5579b7a01a6b8
Victor Ndaba <mwangindaba@gmail.com>
2023-01-04 20:25:31 +0300

    refactor(create-bf): updated create-bf pkg

--- packages/create-bf/src/lib/create.ts
+++ packages/create-bf/src/lib/create.ts
@@ -1,9 +1,11 @@
-import chalkAnimation from "chalk-animation";
 import fs from "fs";
+import { cyan } from "kleur/colors";
+import { Listr } from "listr2";
 import path from "path";
 import { fileURLToPath } from "url";
 import { isMinimalTrack, isTemplateTrack } from "../prompts/utils";
-import { copy, emptyDir, IConfig, rocketAscii } from "./utils";
+import { pacman } from "./pm";
+import { copy, emptyDir, IConfig } from "./utils";
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -19,54 +21,70 @@ export async function create(cfg: IConfig) {
   }
 }
 
-const sleep = (ms = 200) => new Promise((r) => setTimeout(r, ms));
+// const sleep = (ms = 200) => new Promise((r) => setTimeout(r, ms));
 
 async function createMinimalApp(cfg: IConfig) {
-  const rainbow = chalkAnimation.rainbow(
-    `${rocketAscii} Creating project files...\n`
-  );
+  const tasks = new Listr([]);
 
-  await sleep(3000);
-  const cwd = process.cwd();
-  const root = path.join(cwd, cfg.targetDir);
+  tasks.add({
+    title: "Copying project files",
+    task: async () => {
+      const cwd = process.cwd();
+      const root = path.join(cwd, cfg.targetDir);
 
-  if (cfg.overwrite) {
-    emptyDir(root);
-  } else if (!fs.existsSync(root)) {
-    fs.mkdirSync(root, { recursive: true });
+      if (cfg.overwrite) {
+        emptyDir(root);
+      } else if (!fs.existsSync(root)) {
+        fs.mkdirSync(root, { recursive: true });
+      }
+
+      const templateDir = path.resolve(
+        __filename,
+        "../../templates",
+        `minimal-${cfg.languageVariant}`
+      );
+
+      const renameFiles: Record<string, string | undefined> = {
+        _gitignore: ".gitignore",
+      };
+
+      const write = (file: string, content?: string) => {
+        const targetPath = path.join(root, renameFiles[file] ?? file);
+        if (content) {
+          fs.writeFileSync(targetPath, content);
+        } else {
+          copy(path.join(templateDir, file), targetPath);
+        }
+      };
+
+      const files = fs.readdirSync(templateDir);
+      for (const file of files.filter((f) => f !== "package.json")) {
+        write(file);
+      }
+
+      const pkg = JSON.parse(
+        fs.readFileSync(path.join(templateDir, "package.json"), "utf-8")
+      );
+
+      pkg.name = cfg.projectName;
+
+      write("package.json", JSON.stringify(pkg, null, 2));
+    },
+  });
+
+  if (cfg.installDeps) {
+    tasks.add({
+      title: "Installing dependencies",
+      task: async () => {
+        await pacman.install();
+      },
+    });
   }
 
-  const templateDir = path.resolve(
-    __filename,
-    "../../templates",
-    `minimal-${cfg.languageVariant}`
-  );
-
-  const renameFiles: Record<string, string | undefined> = {
-    _gitignore: ".gitignore",
-  };
-
-  const write = (file: string, content?: string) => {
-    const targetPath = path.join(root, renameFiles[file] ?? file);
-    if (content) {
-      fs.writeFileSync(targetPath, content);
-    } else {
-      copy(path.join(templateDir, file), targetPath);
-    }
-  };
-
-  const files = fs.readdirSync(templateDir);
-  for (const file of files.filter((f) => f !== "package.json")) {
-    write(file);
-  }
-
-  const pkg = JSON.parse(
-    fs.readFileSync(path.join(templateDir, "package.json"), "utf-8")
-  );
-
-  pkg.name = cfg.projectName;
-
-  write("package.json", JSON.stringify(pkg, null, 2));
-
-  rainbow.stop();
+  await tasks.run();
+  // show success message
+  console.log(cyan("Success!"));
+  console.log(cyan("Run the following commands to get started:"));
+  console.log(`   cd ${cfg.targetDir}`);
+  console.log(`   ${pacman.name} run dev`);
 }
