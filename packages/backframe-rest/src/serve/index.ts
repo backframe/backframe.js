940681acce2aae02c3b028b895591e510ba70b41
Victor Ndaba <mwangindaba@gmail.com>
2023-01-05 08:13:00 +0300

    perf(rest): cleaned up prod server

--- packages/backframe-rest/src/serve/index.ts
+++ packages/backframe-rest/src/serve/index.ts
@@ -1,12 +1,11 @@
 import { BfConfig } from "@backframe/core";
-import { loadModule, logger, resolveCwd } from "@backframe/utils";
+import { loadModule, logger } from "@backframe/utils";
 import { BfServer } from "../app/index.js";
 
 export async function startServer(config: BfConfig, port?: number) {
-  const root = config.getRootDirName();
-  const entry = config.getEntryPointName();
+  const entry = config.getEntryPoint();
 
-  const file = await loadModule(resolveCwd(root, entry));
+  const file = await loadModule(entry);
   if (!file.default) {
     logger.error(`expected a default export from ${entry}`);
     process.exit(1);
