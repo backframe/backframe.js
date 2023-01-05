615034436818404b381d719b86e755e25ffc052c
Victor Ndaba <mwangindaba@gmail.com>
2023-01-05 08:15:49 +0300

    feat(rest): created default handlers functionality

--- packages/backframe-rest/src/app/handlers.ts
+++ packages/backframe-rest/src/app/handlers.ts
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/ban-types  */
 
+import { BfConfig } from "@backframe/core";
+import { DB, DbEntry } from "@backframe/models";
 import { ZodRawShape } from "zod";
+import { GenericException } from "../lib/errors.js";
 import { Handler, IHandlerConfig, IHandlers, Method } from "../lib/types.js";
+import { Resource } from "./resources.js";
 
 export function defineHandlers() {
   return new ResourceModule();
@@ -12,7 +16,7 @@ export function createHandler<T extends ZodRawShape>(h: IHandlerConfig<T>) {
 }
 
 export class ResourceModule {
-  __middleware?: Handler<{}>[];
+  __middleware?: Handler<unknown, {}>[];
   __handlers?: IHandlers;
 
   constructor() {
@@ -25,7 +29,7 @@ export class ResourceModule {
     return this;
   }
 
-  middleware(m: Handler<{}>) {
+  middleware(m: Handler<unknown, {}>) {
     // resource level middleware
     this.__middleware?.push(m);
     return this;
@@ -43,4 +47,156 @@ export function _getStaticHandler(method: Method, route: string) {
       });
     },
   });
+}
+
+export class DefaultHandlers<T> {
+  #db: DB;
+  #model: string;
+  #dbObject: DbEntry<unknown>;
+
+  constructor(r: Resource<T>, bfConfig: BfConfig) {
+    this.#db = bfConfig.database as DB;
+    this.#model = (r.model as string) ?? r.route;
+    this.#dbObject = this.#db[this.#model] ?? this.#db[r.routeItem.dirname];
+  }
+
+  GET() {
+    const dbObject = this.#dbObject;
+    return createHandler({
+      async action(ctx) {
+        let data;
+
+        // check if [param] exists
+        const k = Object.keys(ctx.params)[0] ?? "";
+        const v = Object.values(ctx.params)[0] ?? "";
+
+        if (k && v) {
+          // get one from collection
+          try {
+            data = await dbObject.findUnique({
+              where: {
+                [k]: v,
+              },
+            });
+          } catch (error) {
+            return ctx.json(
+              {
+                status: "Error",
+                msg: error.message ?? "An error occurred.",
+              },
+              400
+            );
+          }
+        } else {
+          // get multiple objects from collection
+          // TODO: update pagination options
+          try {
+            const values = await dbObject.findMany({
+              skip: Number(ctx.query.skip ?? 0),
+              take: Number(ctx.query.take ?? 10),
+              orderBy: ctx.query.orderBy ?? undefined,
+            });
+            data = {
+              count: values.length,
+              page: Number(ctx.query.skip ?? 0),
+              data: values,
+            };
+          } catch (error) {
+            return ctx.json(
+              {
+                status: "Error",
+                msg: error.message ?? "An error occurred.",
+              },
+              400
+            );
+          }
+        }
+
+        return ctx.json(data as object);
+      },
+    });
+  }
+
+  POST() {
+    const dbObject = this.#dbObject;
+    return createHandler({
+      async action(ctx) {
+        try {
+          const item = await dbObject.create({ data: ctx.input });
+          return ctx.json(item as object);
+        } catch (error) {
+          return ctx.json(new GenericException(400, error.message, ""), 400);
+        }
+      },
+    });
+  }
+
+  PUT() {
+    const dbObject = this.#dbObject;
+    return createHandler({
+      async action(ctx) {
+        try {
+          const item = await dbObject.update({
+            where: {
+              id: ctx.params.id,
+            },
+            data: ctx.input,
+          });
+          return ctx.json(item as object);
+        } catch (error) {
+          return ctx.json(new GenericException(400, error.message, ""), 400);
+        }
+      },
+    });
+  }
+
+  DELETE() {
+    const dbObject = this.#dbObject;
+    return createHandler({
+      async action(ctx) {
+        try {
+          const item = await dbObject.delete({
+            where: {
+              id: ctx.params.id,
+            },
+          });
+          return ctx.json(item as object);
+        } catch (error) {
+          return ctx.json(new GenericException(400, error.message, ""), 400);
+        }
+      },
+    });
+  }
+
+  PATCH() {
+    const dbObject = this.#dbObject;
+    return createHandler({
+      async action(ctx) {
+        try {
+          const item = await dbObject.update({
+            where: {
+              id: ctx.params.id,
+            },
+            data: ctx.input,
+          });
+          return ctx.json(item as object);
+        } catch (error) {
+          return ctx.json(new GenericException(400, error.message, ""), 400);
+        }
+      },
+    });
+  }
+
+  STATIC(method: Method, route: string) {
+    const m = method.toUpperCase();
+    return createHandler({
+      action(ctx) {
+        return ctx.json({
+          status: "Okay",
+          msg: `The \`${m}\` method for the \`${route}\` route is working successfully.`,
+          body: `This is a default static handler. It can be overriden by defining your own handler for the \`${m}\` method or by defining a model in the config of the \`${route}\` route.`,
+        });
+      },
+    });
+  }
 }
