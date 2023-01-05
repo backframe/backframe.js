9e8d422045db19e15943fb56184f74f79d0c49f7
Victor Ndaba <mwangindaba@gmail.com>
2023-01-06 02:43:48 +0300

    docs(core): added type defs for plugins cfg

--- packages/backframe-core/src/plugins/index.ts
+++ packages/backframe-core/src/plugins/index.ts
@@ -3,61 +3,109 @@ import { BfConfig } from "../config/index.js";
 export type Plugin = (args: unknown) => BfPluginConfig;
 export type PluginListener = (cfg: BfConfig) => void;
 
-export interface BfPluginConfig {
+// this are hooks that can be used by the plugin to modify the behavior of the app by `hooking` in at different points of the app lifecycle
+export type PluginHooks = {
   /**
-   * @docs
-   * @optional
-   * name - the name of the plugin
+   * This hook is called immediately after the `BfUserConfig` has been parsed and the `BfConfig` object is created. This is the first hook that is called. This hook is useful if you want to modify the `BfConfig` object before it is used by the rest of the application. For example, you can use this hook to add additional `BfConfig` properties that are not defined elsewhere.
+   * @link https://backframe.github.io/js/docs/plugins/config#onconfiginit
+   * @param cfg The `BfConfig` object
+   * @example
+   * import { defineConfig, type BfConfig } from "@backframe/core";
    *
+   * function myPlugin() {
+   *   return {
+   *      name: "my-plugin",
+   *      onConfigInit: (cfg: BfConfig) => {
+   *         cfg.pluginsOptions.myPlugin = {
+   *            // mount plugin-specific config on the BfConfig object
+   *          }
+   *      }
+   *   }
+   * }
+   *
+   * export default defineConfig({
+   *    // ...
+   *    plugins: [myPlugin()]
+   *    // ...
+   * })
+   */
+  onConfigInit?: PluginListener;
+  /**
+   * The `onServerInit` hook is called after the `BfConfig` object has been created and the `BfServer` object has been initialized but not started yet. It is in this step that resources are added to the server, middleware applied and other server configuration is done. This hook is useful if you want to modify the `BfServer` object before it is started.
+   * @link https://backframe.github.io/js/docs/plugins/config#onserverinit
+   * @param cfg The `BfConfig` object
+   * @example
+   * import { defineConfig, type BfConfig } from "@backframe/core";
+   *
+   * function myPlugin() {
+   *    return {
+   *      name: "my-plugin",
+   *      onServerInit: (cfg: BfConfig) => {
+   *         cfg.server.middleware(function(ctx) {
+   *           // ... do something with the ctx object
+   *         })
+   *      })
+   *    }
+   * }
+   *
+   * export default defineConfig({
+   *    // ...
+   *    plugins: [myPlugin()]
+   *    // ...
+   * })
+   */
+  onServerInit?: PluginListener;
+  /**
+   * The `onServerStart` hook is called after the `BfServer` object has been started. This hook is useful if you want to do something after the server has started.
+   * @link https://backframe.github.io/js/docs/plugins/config#onserverstart
+   * @param cfg The `BfConfig` object
+   * @example
+   * import { defineConfig, type BfConfig, logger } from "@backframe/core";
+   *
+   * function myPlugin() {
+   *   return {
+   *      name: "my-plugin",
+   *      onServerStart: (cfg: BfConfig) => {
+   *        logger.info("this will be printed after the server has started")
+   *      }
+   *   }
+   * }
+   *
+   * export default defineConfig({
+   *  // ...
+   * plugins: [myPlugin()]
+   * // ...
+   * })
+   *
+   */
+  onServerStart?: PluginListener;
+  onServerStop?: PluginListener;
+  onSocketsInit?: PluginListener;
+  emailProvider?: PluginListener;
+  smsProvider?: PluginListener;
+  pushProvider?: PluginListener;
+  storageProvider?: PluginListener;
+  compiler?: PluginListener;
+};
+
+export type BfPluginConfig = {
+  /**
+   * This is the name assigned to the plugin. This is dispayed to the user when an issue is found with a plugin or general reporting.
+   * @link https://backframe.github.io/js/docs/plugins/config#name
    * @default `unknown-plugin-$randID`
    */
   name?: string;
   /**
-   * @docs
-   * @optional
-   * description - a simple description of the plugin
-   * these values are used to declare your plugin in the plugin manifest
-   *
+   * This is a simple description of the plugin. This is dispayed to the user when an issue is found with a plugin or general reporting.
+   * @link https://backframe.github.io/js/docs/plugins/config#description
    * @default `unknown-plugin-$randID`
+   *
    */
   description?: string;
   /**
-   * @docs
-   * @optional
-   *
-   * modify server listener - this is a helper method that allows you to declare a function that will be invoked after the server has been created but before it's started
-   *
-   * @example
-   * ```js
-   * {
-   *    // ...,
-   *    modifyServer(cfg) {
-   *      // get access to the internal express app
-   *      const app = cfg.server._app
-   *
-   *      // mount a new route
-   *      app.get("/ping", (_rq, rs) => rs.send("pong"))
-   *    }
-   * }
+   * This is the version of the plugin. This is dispayed to the user when an issue is found with a plugin or general reporting.
+   * @link https://backframe.github.io/js/docs/plugins/config#version
+   * @default `unknown-plugin-$randID`
    */
-  modifyServer?: PluginListener;
-  /**
-   * @docs
-   *
-   */
-  emailProvider?: PluginListener;
-  /**
-   * @docs
-   *
-   */
-  storageProvider?: PluginListener;
-  /**
-   * @docs
-   *
-   */
-  compiler?: PluginListener;
-  /**
-   * @docs
-   */
-  modifyConfig?: PluginListener;
-}
+  version?: string;
+} & PluginHooks;
