0883b69a4654885212803386abe174cca7d2faee
Victor Ndaba <mwangindaba@gmail.com>
2023-01-06 06:22:57 +0300

    chore: modified pkg json versions

--- packages/backframe-graphql/src/index.ts
+++ packages/backframe-graphql/src/index.ts
@@ -1,17 +1,68 @@
-export class SchemaConfig {
-  #query: [];
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { GraphQLObjectType, GraphQLSchema } from "graphql";
+
+export class SchemaConfig<C> {
+  #schema: GraphQLSchema;
+  #types: GraphQLObjectType[];
+  #queries: [];
   #mutations: [];
   #subscriptions: [];
   #directives: [];
 
-  query<T, U>(
-    _name: string,
-    _cfg: {
-      input?: T;
+  query<T, U extends GraphQLObjectType>(
+    name: string,
+    cfg: {
+      args?: T;
       schema: U;
-      resolve: (parent: unknown, args: T, ctx: unknown, info: unknown) => void;
+      resolve: (parent: unknown, args: T, ctx: C, info: unknown) => void;
     }
   ) {
-    //
+    //TODO: Validate name
+    const _type = new GraphQLObjectType({
+      name,
+      fields: {},
+    });
+    if (!this.#types.includes(cfg.schema)) {
+      this.#types.push(cfg.schema); // register new schema
+    }
   }
+}
+
+export { g } from "./definitions/index.js";
+export * from "./definitions/types.js";
+
+export function gql() {
+  //
+}
+
+export function createObject() {
+  //
+}
+
+export function createInput() {
+  //
+}
+
+export function createResolver() {
+  //
+}
+
+export function createQuery() {
+  //
+}
+
+export function createMutation() {
+  //
+}
+
+export function createSubscription() {
+  //
+}
+
+export function createDirective() {
+  //
+}
+
+export function defineSchema() {
+  //
 }
