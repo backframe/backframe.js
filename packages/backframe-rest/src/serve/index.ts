ee3426d8f82a559834907745c3e4c38f5677c11b
Victor Ndaba <mwangindaba@gmail.com>
2023-01-07 06:42:27 +0300

    fix(rest): patched errs in `rest/serve`

--- packages/backframe-rest/src/serve/index.ts
+++ packages/backframe-rest/src/serve/index.ts
@@ -7,11 +7,15 @@ const argv = process.argv.slice(2);
 
 export async function startServer(
   config: BfConfig,
-  port = +argv[0] ?? process.env.PORT ? +process.env.PORT : undefined
+  port = process.env.PORT ? +process.env.PORT : undefined
 ) {
-  const entry = config.getAbsDirPath("entryPoint");
+  if (argv[0] && argv[0] !== "undefined") {
+    port = +argv[0];
+  }
 
+  const entry = config.getAbsDirPath("entryPoint");
   const file = await loadModule(entry);
+
   if (!file.default) {
     logger.error(`expected a default export from ${entry}`);
     process.exit(1);
