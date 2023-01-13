18e5834bcf1ecdfabef4e0b49401902eb8f483b4
Victor Ndaba <mwangindaba@gmail.com>
2023-01-14 00:38:10 +0300

    feat: added astro mdx integration

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,7 +1,9 @@
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), mdx()]
 });
