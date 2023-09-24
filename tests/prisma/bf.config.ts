c4f0614d462e4ed284023f7b3de10850b10cacb9
Victor Ndaba <mwangindaba@gmail.com>
2023-09-25 02:39:29 +0300

    test: updated test workflows

--- tests/prisma/bf.config.ts
+++ tests/prisma/bf.config.ts
@@ -1,13 +1,43 @@
 import { PrismaAdapter } from "@backframe/adapter-prisma";
-import { defineConfig } from "@backframe/core";
+import auth from "@backframe/auth";
+import africastalking from "@backframe/auth/integrations/africastalking";
+import credentials from "@backframe/auth/providers/credentials";
+import { createEnvValidator, defineConfig, z } from "@backframe/core";
 import { PrismaClient } from "@bf/database";
 
 const client = new PrismaClient();
 const database = new PrismaAdapter(client);
+
+export const env = createEnvValidator({
+  schema: z.object({
+    AFRICASTALKING_API_KEY: z.string().optional(),
+    AFRICASTALKING_USERNAME: z.string().optional(),
+    AFRICASTALKING_SENDER_ID: z.string().optional(),
+  }),
+});
 
 export default defineConfig({
   interfaces: {
     rest: {},
   },
   database,
+  plugins: [
+    auth({
+      mfaConfiguration: {
+        status: "OFF",
+        mfaTypes: ["SMS", "TOTP"],
+      },
+      requiredAttributes: ["firstName", "lastName", "email"],
+      allowedSignInAttributes: ["phone"],
+    }),
+    africastalking({
+      apiKey: env.AFRICASTALKING_API_KEY,
+      username: env.AFRICASTALKING_USERNAME,
+      senderId: env.AFRICASTALKING_SENDER_ID,
+    }),
+  ],
+  authentication: {
+    strategy: "token-based",
+    providers: [credentials()],
+  },
 });
